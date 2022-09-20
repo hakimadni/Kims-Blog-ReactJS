84ec7aca94e3b1acdc5807a69be4090a302c31d7
Hakim <hakim.adni@gmail.com>
2022-09-20 17:20:34 +0700

    Add my first reactjs project

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+const Home = () => {
+    const handleClick = (e)=> {
+        console.log("Hello",e)
+    }
+    const handleClickAgain = (name,e)=> {
+        console.log("Hello "+ name, e.target);
+    }
+
+    return ( 
+        <div className="home">
+            <h2>
+                Homepage
+            </h2>
+            <button onClick={handleClick} >Click me</button>
+            <button onClick={(e)=>handleClickAgain('Hakim',e)} >Click me again</button>
+        </div>
+     );
+}
+ 
+export default Home;
